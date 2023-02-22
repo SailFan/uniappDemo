ca041cd22ef5acac023687464237ee9f0a32309b
fan <wf.liu@hotmial.com>
2023-02-22 17:09:50 +0800

    完善轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,9 @@
 <template>
-	<view >
-		HOME
+	<view class="home">
+		<swiper>
+			<swiper-item>1</swiper-item>
+			<swiper-item>1</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,14 +11,26 @@
 	export default {
 		data() {
 			return {
-				
+				swipers: ""
 			}
 		},
 		onLoad() {
-
+			this.getSwipers()
 		},
 		methods: {
-
+			getSwipers(){
+				uni.request({
+					url: '/data/banner.json',
+					success(res) {
+						if(res.data.code!==1){
+							return uni.showToast({
+								title: "获取数据失败"
+							})
+						}
+						this.swipers=res.data.data
+					}
+				})
+			}
 		}
 	}
 </script>
