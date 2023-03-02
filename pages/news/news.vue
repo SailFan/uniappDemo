f8e5d4ef779471975a77edf97bfbb83890b194f8
fan <wf.liu@hotmial.com>
2023-03-02 11:33:43 +0800

    fied

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -1,12 +1,12 @@
 <template>
 	<view class="news">
-		<view class="new_item">
-			<image src="../../static/product/1.png"></image>
-			<view>
-				<view class="title">hhahhafhahsfdhfhsdafhhdasf</view>
+		<view class="new_item" v-for="item in list" :key="item.id">
+			<image :src="item.image_url"></image>
+			<view class="right">
+				<view class="title">{{item.title}}</view>
 				<view class="info">
-					<text>ewqrqwer</text>
-					<text>ewqrqwerewqrq</text>
+					<text> 45 </text>
+					<text>20210212</text>
 				</view>
 			</view>
 		</view>
@@ -17,15 +17,57 @@
 	export default {
 		data() {
 			return {
-				
+				list: []
 			}
 		},
+		onLoad() {
+			this.getdetails()
+		},
 		methods: {
-			
+			getdetails() {
+				uni.request({
+					url:'/data/details.json',
+					success: (res) => {
+						if(res.data.code!==1){
+							return uni.showToast({
+								title: "获取数据失败"
+							})
+						}
+					this.list = res.data.message
+					}
+				})
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.news {
+		.new_item {
+			display: flex;
+			padding: 10rpx 20rpx;
+			border-bottom: 1px solid #b50e03;;
+			image {
+				width: 200rpx;
+				height: 150rpx;
+				min-width: 200rpx;
+				max-width: 200rpx;
+			}
+			.right{
+				display: flex;
+				flex-direction: column;
+				margin-left: 15rpx;
+				justify-content: space-between;
+				.title {
+					font-size: 33rpx;
+				}
+				.info {
+					font-size: 24rpx;
+					text:nth-child(2){
+						margin-left: 30rpx;
+					}
+				}
+			}
+		}
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/news/news.js
+++ unpackage/dist/dev/mp-weixin/pages/news/news.js
@@ -2,12 +2,39 @@
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      list: []
+    };
   },
-  methods: {}
+  onLoad() {
+    this.getdetails();
+  },
+  methods: {
+    getdetails() {
+      common_vendor.index.request({
+        url: "/data/details.json",
+        success: (res) => {
+          if (res.data.code !== 1) {
+            return common_vendor.index.showToast({
+              title: "\u83B7\u53D6\u6570\u636E\u5931\u8D25"
+            });
+          }
+          this.list = res.data.message;
+        }
+      });
+    }
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.f($data.list, (item, k0, i0) => {
+      return {
+        a: item.image_url,
+        b: common_vendor.t(item.title),
+        c: item.id
+      };
+    })
+  };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/yz/Documents/HBuilderProjects/uniappDemo/pages/news/news.vue"]]);
 wx.createPage(MiniProgramPage);
